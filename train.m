c5009e35755b03a82412037f026952e1d1586202
eglxiang <cnwhu@yahoo.com>
2016-07-14 15:57:22 -0400

    format of saved txt
    replace \t with space

--- train.m
+++ train.m
@@ -63,7 +63,7 @@ cd(initial_directory);
 [ L b info ] = ldml_learn(A,y);
 % save the projection matrix to txt
 fileID = fopen('ProjectTo650Mat.txt','w');
-fprintf(fileID, [repmat('%f\t',1,size(L,2)) '\n'], L');
+fprintf(fileID, [repmat('%f ',1,size(L,2)) '\n'], L');
 fclose(fileID);
 
 %% Testing: whether two sequence represent the same person
